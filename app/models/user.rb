45e74cd4208f4764666533dd621de25ae479dd61
Joseph Bouchard <jbouchard@inalabgroup.com>
2024-08-22 14:46:49 -0500

    Associate users and entries
    Track the user who created an entry and only display entries to the user who created them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :entries
+
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
 
   def self.from_omniauth(access_token)
